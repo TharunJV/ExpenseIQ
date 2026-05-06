7e7537889a8b51566ae22d2a67961a662a884757
Tharun J V <jvtharun2007@gmail.com>
2026-05-06 14:36:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ CORS(app)
 # ─────────────────────────────────────────
 
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT")),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASS"),
-    "database": os.getenv("DB_NAME"),
+    "host": os.getenv("MYSQLHOST"),
+    "user": os.getenv("MYSQLUSER"),
+    "password": os.getenv("MYSQLPASSWORD"),
+    "database": os.getenv("MYSQLDATABASE"),
+    "port": int(os.getenv("MYSQLPORT"))
 }
 
 # Family members — edit names here to add/remove members
